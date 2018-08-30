b13bf477bb16a2d119cd230574de28a7ddef6645
Kirk Sullivan <kirk_135@hotmail.com>
2018-08-29 23:52:06 -0400

    Clairty

--- sherlockAndAnagrams/soultion.js
+++ sherlockAndAnagrams/soultion.js
@@ -1,17 +1,17 @@
 function sherlockAndAnagrams(s) {
   let totalAnagrams = 0;
-  let subStrings = {}
+  let hashOfStrings = {}
   if(s.length <= 1) return 0;
   for(let i = 0; i < s.length; i++){
       for(let j = i; j< s.length; j++){
           let tempstring = s.substring(i,j+1).split("").sort().join("");
-          if(subStrings[tempstring]) subStrings[tempstring] += 1
-          else subStrings[tempstring] = 1
+          if(hashOfStrings[tempstring]) hashOfStrings[tempstring] += 1
+          else hashOfStrings[tempstring] = 1
       }
   }
-  for(let k in subStrings) {
-      if(subStrings[k] > 1) {
-          let temp = (subStrings[k]) * (subStrings[k] -1)/2
+  for(let k in hashOfStrings) {
+      if(hashOfStrings[k] > 1) {
+          let temp = (hashOfStrings[k]) * (hashOfStrings[k] -1)/2
           totalAnagrams += temp
       }
   }
